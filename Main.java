15723f4fd4f2732d1b82c29dd22f23aabfc401f5
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2022-10-22 21:46:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+class Main {
+  public static void main(String[] args) {
+
+    int i=50;
+    
+    for(i=50;i<=100;i++){
+    
+    if(i%2==0){
+  System.out.println("Par: "+i);
+  } else{
+  System.out.println("Impar: "+i);
+  }
+    }
+}
+}
